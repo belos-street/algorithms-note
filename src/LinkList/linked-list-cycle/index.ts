f04640fcb761dd09f8f45604528dda90ad6dc866
aa1213889 <869135421@qq.com>
2024-05-11 00:53:45 +0800

    feat:146

---
+++ src/LinkList/linked-list-cycle/index.ts
@@ -0,0 +1,28 @@
+/** https://leetcode.cn/problems/linked-list-cycle */
+import { ListNode } from '..'
+
+export function hasCycle(head: ListNode | null): boolean {
+  const cache = new Set()
+  while (head) {
+    if (cache.has(head)) return true
+    cache.add(head)
+    head = head.next
+  }
+  return false
+}
+
+/**
+ * @function 双指针解法
+ * @param slow 慢指针 一次走一个
+ * @param fast 快指针 一次走两个
+ * 如果有环形，一定会撞到
+ */
+export function hasCycle_V2(head: ListNode | null): boolean {
+  let [slow, fast] = [head, head]
+  while (fast?.next && fast) {
+    slow = slow!.next
+    fast = fast.next!.next
+    if (slow === fast) return true
+  }
+  return false
+}
---
+++ src/LinkList/remove-linked-list-elements/index.ts
@@ -0,0 +1,19 @@
+/** https://leetcode.cn/problems/remove-linked-list-elements/ */
+import { ListNode } from '..'
+
+export function removeElements(head: ListNode | null, val: number): ListNode | null {
+  let eleNode = {
+    next: head
+  }
+
+  let p = eleNode
+  while (p.next) {
+    if (p.next.val === val) {
+      p.next = p.next.next
+    } else {
+      p = p.next
+    }
+  }
+
+  return eleNode.next
+}
