dae9bf1382a80359f24f2a8edaaccc9a4d8a3e54
aa1213889 <869135421@qq.com>
2024-05-30 01:06:00 +0800

    还有bug

--- src/DynamicProgramming/longest-increasing-subsequence/index.ts
+++ src/DynamicProgramming/longest-increasing-subsequence/index.ts
@@ -1,26 +1,43 @@
 /** @link https://leetcode.cn/problems/longest-increasing-subsequence */
 
-export function lengthOfLIS(nums: number[]): number {
-  if (nums.length === 0) return 0
+export function lengthOfLIS(nums: number[]): number[] {
+  if (nums.length === 0) return []
 
   const dp: number[] = []
   dp[0] = nums[0]
 
+  //[4, 10, 4, 3, 8, 9]
+  // dp
+  // 4
+  // 4 10
+  // 4 4
   for (let i = 1; i < nums.length; i++) {
     const num = nums[i]
     const dpLastNum = dp[dp.length - 1]
 
-    if (dp.includes(num)) continue
-
-    if (num < dpLastNum) {
-      dp[dp.length - 1] = num
-    }
+    if (dp.includes(num)) continue //等于
 
     if (num > dpLastNum) {
+      //大于
       dp.push(num)
+      continue
     }
+
+    //小于
+    // dp[dp.length - 1] = num
+
+    let dpIndex = dp.length - 1
+    while (dpIndex >= 0) {
+      if (dp[dpIndex] <= num) {
+        dp[dpIndex] = NaN
+      } else {
+        break
+      }
+      dpIndex--
+    }
+
     console.log(dp)
   }
 
-  return dp.length
+  return dp
 }
--- src/DynamicProgramming/longest-increasing-subsequence/test.spec.ts
+++ src/DynamicProgramming/longest-increasing-subsequence/test.spec.ts
@@ -3,18 +3,39 @@ import { lengthOfLIS } from '.'
 
 describe('最长递增子序列', () => {
   it('example1', () => {
-    expect(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])).toBe(4)
+    expect(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])).toEqual([2, 3, 7, 18])
   })
+  // 10
+  // 9
+  // 2
+  // 2 5
+  // 2 3
+  // 2 3 7
+  // 2 3 7 101
+  // 2 3 7 18
 
   it('example2', () => {
-    expect(lengthOfLIS([0, 1, 0, 3, 2, 3])).toBe(4)
+    expect(lengthOfLIS([0, 1, 0, 3, 2, 3])).toEqual([0, 1, 2, 3])
   })
+  // 0
+  // 0 1
+  // 0 1
+  // 0 1 3
+  // 0 1 2
+  // 0 1 2 3
 
   it('example3', () => {
-    expect(lengthOfLIS([7, 7, 7, 7, 7, 7, 7])).toBe(1)
+    expect(lengthOfLIS([7, 7, 7, 7, 7, 7, 7])).toEqual([7])
   })
 
   it('example4', () => {
-    expect(lengthOfLIS([4, 10, 4, 3, 8, 9])).toBe(3)
+    expect(lengthOfLIS([4, 10, 4, 3, 8, 9])).toEqual([3, 8, 9])
   })
+  //         大于最后一个才push  小于最后一个只替换
+  // [4]
+  // [4,10] 10  +
+  // [4,10]            替换第0个4
+  // [3,10]
+  // 3,8
+  // 3,8,9
 })
