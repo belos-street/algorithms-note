03f8f06dc179abb2c55511caddaa80e6c5136958
zhi.jiang <zhi.jiang@sectrend.com.cn>
2024-05-30 11:59:06 +0800

    test

--- src/DynamicProgramming/longest-increasing-subsequence/index.ts
+++ src/DynamicProgramming/longest-increasing-subsequence/index.ts
@@ -6,37 +6,36 @@ export function lengthOfLIS(nums: number[]): number[] {
   const dp: number[] = []
   dp[0] = nums[0]
 
-  //[4, 10, 4, 3, 8, 9]
-  // dp
-  // 4
-  // 4 10
-  // 4 4
   for (let i = 1; i < nums.length; i++) {
     const num = nums[i]
     const dpLastNum = dp[dp.length - 1]
 
     if (dp.includes(num)) continue //等于
 
+    //大于
     if (num > dpLastNum) {
-      //大于
       dp.push(num)
       continue
     }
 
-    //小于
-    // dp[dp.length - 1] = num
-
+    //可以用二分法优化
     let dpIndex = dp.length - 1
     while (dpIndex >= 0) {
-      if (dp[dpIndex] <= num) {
-        dp[dpIndex] = NaN
+      if (dp[dpIndex] === num) break //等于
+
+      const preNum = dp[dpIndex - 1]
+      if (preNum) {
+        if (num > preNum && num < dp[dpIndex]) {
+          dp[dpIndex] = num
+          break
+        }
       } else {
+        dp[dpIndex] = num
         break
       }
+
       dpIndex--
     }
-
-    console.log(dp)
   }
 
   return dp
